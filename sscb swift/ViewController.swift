71e8f50169c6209b93b372650158bdbf43f84758
to <sami@pangc-pc-010.local>
2018-06-01 13:53:57 +0900

    Initial Commit

---
+++ sscb swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sscb swift
+//
+//  Created by sami on 2018/06/01.
+//  Copyright © 2018年 pancristal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
